7fe659cf2e111243d00e800829a201739a3ece04
MaminiainaIndrafo <maminiainaindrafo@gmail.com>
2026-06-30 10:28:33 +0300

    Initial Commit

---
+++ OdcApp/OdcAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OdcAppApp.swift
+//  OdcApp
+//
+//  Created by Maminiaina Indrafo on 30/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct OdcAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
